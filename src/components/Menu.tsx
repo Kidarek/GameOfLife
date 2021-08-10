d0f53effebda1a18a388277d0ee0f1cbae6ebfb0
Kidarek <evilace0@gmail.com>
2021-08-10 02:20:05 -0400

    Removed marks from slider

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,17 +11,6 @@ interface MenuProps {
     setPlayTime: (playTime: number) => void
 }
 
-const marks = [
-    {
-        value: 50,
-        label: "50ms"
-    },
-    {
-        value: 1000,
-        label: "1s"
-    }
-]
-
 export default function SimpleMenu(props: MenuProps): JSX.Element {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
@@ -39,32 +28,31 @@ export default function SimpleMenu(props: MenuProps): JSX.Element {
                 <SettingsIcon></SettingsIcon>
             </Button>
             <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-                  <TextField
-                      label="Board Size"
-                      type="number"
-                      InputProps={{ inputProps: { min: 1 } }}
-                      defaultValue={props.size}
-                      onChange={(e) => props.sizeOnChange(parseInt(e.target.value))}
-                  />
-                  <TextField
-                      label="Percentage of squares filled"
-                      type="number"
-                      InputProps={{
-                          inputProps: { min: 0, max: 100 },
-                          endAdornment: <InputAdornment position="end">%</InputAdornment>
-                      }}
-                      defaultValue={props.randomFillPercent}
-                      onChange={(e) => props.setRandomFillPercent(parseInt(e.target.value))}
-                  />
-                  <Stack direction="column" width='100%' ml={1}>
-                      <Typography>Time Between Moves</Typography>
-                      <Slider
-                          min={50}
-                          max={1000}
-                          defaultValue={props.playTime}
-                          onChange={(_, val) => props.setPlayTime(val as number)}
-                          marks={marks}></Slider>
-                  </Stack>
+                <TextField
+                    label="Board Size"
+                    type="number"
+                    InputProps={{ inputProps: { min: 1 } }}
+                    defaultValue={props.size}
+                    onChange={(e) => props.sizeOnChange(parseInt(e.target.value))}
+                />
+                <TextField
+                    label="Percentage of squares filled"
+                    type="number"
+                    InputProps={{
+                        inputProps: { min: 0, max: 100 },
+                        endAdornment: <InputAdornment position="end">%</InputAdornment>
+                    }}
+                    defaultValue={props.randomFillPercent}
+                    onChange={(e) => props.setRandomFillPercent(parseInt(e.target.value))}
+                />
+                <Stack direction="column" width="100%" ml={1}>
+                    <Typography>Time Between Moves</Typography>
+                    <Slider
+                        min={50}
+                        max={1000}
+                        defaultValue={props.playTime}
+                        onChange={(_, val) => props.setPlayTime(val as number)}></Slider>
+                </Stack>
             </Menu>
         </div>
     )
